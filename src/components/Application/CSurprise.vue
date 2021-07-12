5f3cfee1f55593aea948ea4a65c08b20a2436e14
Katrin Yordanova <katrin.yordanova@crust.tech>
2021-07-12 19:47:20 +0300

    Add controls to component

--- src/components/Application/CSurprise.vue
+++ src/components/Application/CSurprise.vue
@@ -1,13 +1,89 @@
 <template>
-  <c-application-editor-info
-    class="my-5 mx-auto h-50 w-50"
-    :application="application"
-    :processing="info.processing"
-    :success="info.success"
-    :can-create="canCreate"
-    @submit="onInfoSubmit"
-    @delete="onDelete"
-  />
+  <div class="w-100 mt-3">
+    <b-container class="mw-100">
+      <b-row class="mb-5">
+        <b-col cols="3">
+          <aside class="h-100">
+            <p
+              class="font-weight-bold ml-3"
+            >
+              Catalogue
+            </p>
+            <ul
+              v-for="(value, key, index) in components"
+              :key="index"
+              style="list-style: none"
+            >
+              <li
+                v-if="index !== Object.keys(components).length - 1"
+                class="ml-list mt-0"
+              >
+                {{ value.name }}
+              </li>
+            </ul>
+          </aside>
+        </b-col>
+        <b-col cols="8">
+          <c-application-editor-info
+            :application="application"
+            :processing="info.processing"
+            :success="info.success"
+            :can-create="canCreate"
+            @submit="onInfoSubmit"
+            @delete="onDelete"
+          />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <h3>Preset Controls</h3>
+          <ul
+            v-for="(value, key) in [application, info]"
+            :key="key"
+            style="list-style-type: none"
+            class="m-ul"
+          >
+            <li
+              v-for="(v, k) in value"
+              :key="k"
+            >
+              <label>{{ k }}:</label>
+              <span
+                class="text-secondary"
+              >
+                {{ v }}
+              </span>
+            </li>
+          </ul>
+          <label>canCreate:</label>
+          <span class="text-secondary"> {{ canCreate }}</span>
+        </b-col>
+        <b-col>
+          <h3>Controls</h3>
+          <ul
+            v-for="(value, key) in [application, info]"
+            :key="key"
+            style="list-style-type: none"
+            class="m-ul"
+          >
+            <li
+              v-for="(v, k) in value"
+              :key="k"
+            >
+              <label>{{ k }}:</label>
+              <span
+                class="text-secondary"
+              >
+                {{ v }}
+              </span>
+            </li>
+          </ul>
+          <label>canCreate:</label>
+          <span class="text-secondary"> {{ canCreate }}</span>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
@@ -47,7 +123,24 @@ export default {
       application: this.$route.params.application || appData,
       info: this.$route.params.info || { processing: false, success: false },
       canCreate: this.$route.params.canCreate || false,
+      components: this.$options.components,
     }
+  },
+
+  computed: {
+    app: {
+      get () {
+        return [this.application]
+      },
+
+      set (a) {
+        this.$emit('update:application', a)
+      },
+    },
+  },
+
+  created () {
+    console.log(this.data)
   },
 
   methods: {
@@ -58,6 +151,21 @@ export default {
     onDelete () {
       console.log('triggerred onDelete')
     },
+    editApp (a) {
+      console.log(a)
+      // this.$set(r, 'dirty', true)
+    },
   },
 }
 </script>
+
+<style scoped>
+.ml-list {
+  margin-left: -10px;
+}
+
+.m-ul {
+  margin-left: -40px;
+  margin-bottom: -3px;
+}
+</style>
